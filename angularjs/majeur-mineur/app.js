46414d255ad97b36e18f9fd9b5640da58502284c
JuanTrochez <juan.trochez@hotmail.fr>
2015-01-21 19:37:16 +0100

    optimisation

--- angularjs/majeur-mineur/app.js
+++ angularjs/majeur-mineur/app.js
@@ -1,14 +1,13 @@
 (function() {
-    var myApp = angular.module("myApp", []);
+	var myApp = angular.module("myApp", []);
 
-    myApp.controller("getAgeCtrl", [ "$scope", function($scope) {
-        $scope.age = 0;
-        $scope.sayStatus = function() {
-            if ($scope.age < 18) {
-                return "mineur";
-            }
-            
-            return "majeur";
-        };
-    }]);
+	myApp.controller("getAgeCtrl", [ "$scope", function($scope) {
+		$scope.age = 0;
+		$scope.sayStatus = function() {
+			if ($scope.age < 18) {
+				return "mineur";
+			}
+			return "majeur";
+		};
+	}]);
 })();
