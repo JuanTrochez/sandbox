baba0e46cfd35fc4cbbcb3f18a68d0daa4a9110c
JuanTrochez <juan.trochez@hotmail.fr>
2015-01-22 18:57:08 +0100

    modifications

--- angularjs/ng-bind-template/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var app = angular.module("myApp", []);
-
-	app.controller("ExampleCtrl", ["$scope", function($scope) {
-		$scope.title = "Entrez le titre";
-		$scope.content = "Entrez un contenu !";
-	}]);
-})();
