91762997791272124ebacf0515fb612f988b7354
JuanTrochez <juan.trochez@hotmail.fr>
2015-01-22 18:47:24 +0100

    modifications

--- angularjs/ng-copy/app.js
+++ angularjs/ng-copy/app.js
@@ -2,13 +2,10 @@
 	var app = angular.module("myApp", []);
 
 	app.controller("ExampleCtrl", ["$scope", function($scope) {
-		$scope.copy = false;
 		$scope.text = "Copiez-moi";
-		$scope.copiedText = function() {
-			if ($scope.copy == true) {
-				return "Vous avez copié : " + $scope.text;
-			}
-			return "Vous n'avez encore rien copié !";
+		$scope.message = "";
+		$scope.copy = function() {
+			$scope.message = "Vous avez copié : " + $scope.text;
 		};
 	}]);
 })();
