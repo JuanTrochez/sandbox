4bb615b782331895234ddc9361a692be7dfeb817
Juan Trochez <juan.trochez95@gmail.com>
2015-02-05 15:36:31 +0100

    heap

---
+++ c/malloc/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(char **argv, int argc) {
+	printf("Hello world!\n");
+	char *name;
+	for (int i = 0; i < 10000000000; i++) {
+		name = (char *) malloc(100000*sizeof(char));
+		strcpy(name, "coucou");
+		printf("name = %s\n", name);
+		printf("Address = 0x%08x\n", name);
+		free(name);
+	}
+	return 0;
+}
