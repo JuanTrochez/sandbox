df066f9d2e10a8820647f8ee4542224bf25afbc5
JuanTrochez <juan.trochez@hotmail.fr>
2015-02-18 11:19:51 +0100

    ng-value

---
+++ angularjs/ng-value/app.js
@@ -0,0 +1,8 @@
+(function() {
+	var app = angular.module("myApp", []);
+
+	app.controller("ExampleCtrl", ["$scope", function($scope) {
+		$scope.names = ["Arthur", "Kate", "Jhon", "James", "Laura"];
+		$scope.selected = {name: "Kate"};
+	}]);
+})();
