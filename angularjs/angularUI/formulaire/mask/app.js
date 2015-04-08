29d1e75f0d50b25a1b5dc7ffd6fcda66058c1287
Juan Trochez <juan.trochez95@gmail.com>
2015-04-08 13:14:22 +0200

    mask

--- angularjs/angularUI/formulaire/mask/app.js
+++ angularjs/angularUI/formulaire/mask/app.js
@@ -1,10 +1,10 @@
 (function() {
-	"use strict";
+	'use strict';
 
-	var app = angular.module("mainApp", ["ui.mask"]);
+	var app = angular.module('mainApp', [ 'ui.mask' ]);
 
-	app.controller("MainCtrl", ["$scope", function($scope) {
-		$scope.y = '999 - 999 - 999 - 999';
+	app.controller('MainCtrl', [ '$scope', function($scope) {
+		$scope.myMask = '999 - 999 - 999 - 999';
 	}]);
 
 })();
