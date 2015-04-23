602686451f831eac906c615061f34d510cc45056
Juan Trochez <juan.trochez95@gmail.com>
2015-04-23 10:32:52 +0200

    app.js

--- angularjs/native/filter/lower-upper-case/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var app = angular.module('mainApp', []);
-
-	app.controller('MainCtrl', ['$scope', function($scope) {
-		$scope.myText = "A simple text with UPPER and lower case";
-	}]);
-
-})();
