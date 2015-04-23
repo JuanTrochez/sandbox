0a864ca7a127ab63e0c4b2f7e7b50797e1bd05af
Juan Trochez <juan.trochez95@gmail.com>
2015-04-23 14:27:44 +0200

    location

--- angularjs/native/service/location/app.js
+++ angularjs/native/service/location/app.js
@@ -5,48 +5,12 @@
 
 		$scope.url = undefined;
 
-		$scope.locArray = {
-			absUrl: $location.absUrl(),
-			url: $location.url(),
-			protocol: $location.protocol(),
-			host: $location.host(),
-			port: $location.port(),
-			path: $location.path(),
-			search: $location.search(),
-			hash: $location.hash(),
-			//replace: $location.replace(),
-			state: $location.state()
+		$scope.$location = $location;
+
+		$scope.changeUrl = function(url) {
+			$location.path(url);
 		};
 
-		$scope.changeUrl = function(option) {
-			if (option == 'reset') {
-				$location.path('/');
-			} else if (option == 'new') {
-				$location.path('/foo/?bar=baz');
-			} else if (option == 'hash') {
-				$location.path('/foo/?bar=baz#andHash');
-			}
-		};
 
-		$scope.$watchCollection(function() {
-			return $location;
-		}, function(newValue) {
-			$scope.locArray = {
-				absUrl: $location.absUrl(),
-				url: $location.url(),
-				protocol: $location.protocol(),
-				host: $location.host(),
-				port: $location.port(),
-				path: $location.path(),
-				search: $location.search(),
-				hash: $location.hash(),
-				// replace: $location.replace(),
-				// state: $location.state()
-			};
-
-			return $scope.locArray;
-
-		}, true);
-		
 	}]);
 })();
