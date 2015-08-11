4b20f1607294048efe838ad3af29a8a304167bf9
Juan <juan.trochez@hotmail.fr>
2015-08-11 21:07:09 +0200

    template engine

---
+++ node/express/template-engine/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(8080, function() {
+	var host = 'localhost';
+	var port = server.address().port;
+	console.log('server listening at http://%s:%s', host, port);
+});
+
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+	res.render('index', {title: 'Templating with Jade', content: 'First template using Jade'});
+});
+
+
