ec8f3713fd4713489a383f61c3865ad5a8101adf
Juan <juan.trochez@hotmail.fr>
2015-08-21 14:23:44 +0200

    retrieving list from angular with http

--- node/express/example/webapp/public/javascripts/app.js
+++ node/express/example/webapp/public/javascripts/app.js
@@ -3,9 +3,24 @@
 
 	var app = angular.module('mainApp', []);
 
-	app.controller('MainCtrl', ['$injector', function($injector) {
+	app.controller('MainCtrl', ['$injector', '$scope', function($injector, $scope) {
 
-		console.log('controller');
+		var $http = $injector.get('$http');
+
+		$scope.retrieveTodoList = function() {
+			console.log('retrieving data...');
+
+			$http({
+				url: '/todo/get',
+				method: 'get'
+			}).success(function(data) {
+				console.log('data', data);
+				$scope.todoList = data;
+			}).error(function() {
+				console.log('error retrieving todo');
+			});
+		};
+
 
 	}]);
 
--- node/express/example/webapp/routes/index.js
+++ node/express/example/webapp/routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
+// MongoDB
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/todo';
+
+MongoClient.connect(url, function(err, db) {
+	if (err) {
+		console.log('error', err);
+		return
+	}
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
+
+// var insertTodo = function(db, callback) {
+// 	db.collection('todo').insertOne({
+// 		"name": "first todo",
+// 		"isActive": 1
+// 	});
+// };
+
+// MongoClient.connect(url, function(err, db) {
+// 	if (err) {
+// 		console.log('error', err);
+// 		return;
+// 	}
+
+// 	insertTodo(db, function() {
+// 		console.log('inserting todo');
+// 		db.close();
+// 	});
+// });
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo list with Angular, Express and MongoDB' });
+	res.render('index', { title: 'Todo list with Angular, Express and MongoDB' });
+});
+
+
+// Retrieve data from db
+router.get('/todo/get', function(req, res, next) {
+	var result = 
+
+	MongoClient.connect(url, function(err, db) {
+		if (err) {
+			console.log('error', err);
+			return;
+		}
+
+		db.collection('todo').find().toArray(function(err, docs) {
+			res.json(docs);
+		});
+	});
+
 });
 
 module.exports = router;
